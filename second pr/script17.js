c74e49b52c788b49e07c7158ffd210424389ac68
Eugene <66910@mail.com>
2021-10-27 22:35:49 +0300

    3 practize

---
+++ second pr/script17.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const str = "test";
+const arr = [1, 2, 3];
+
+//console.log(str[2]);
+
+let fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseFloat(test));
+console.log(parseInt(test));
